e09c01aab92dd17d32ba4d02706fd4ab8c469cbb
haind <nduchai1998@gmail.com>
2025-07-22 14:21:28 +0700

    a

--- server.js
+++ server.js
@@ -141,7 +141,7 @@ app.post("/download-images", async (req, res) => {
   }
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`🚀 Server đang chạy tại http://localhost:${PORT}`);
 });
